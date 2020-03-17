91dfed2dacb90200a15e23442b2756855b39be59
Jan Petr <honzik.petr@gmail.com>
2020-03-17 13:28:55 +0100

    feat: data endpoint
    Adds endpoint which accepts any file and pushes it to Azure Blob Storage. No authentication, no verification. Just dummy endpoint.

--- src/btwa_api/app/api/endpoints.py
+++ src/btwa_api/app/api/endpoints.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
+import random
+
+from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
 from fastapi.security import HTTPBasicCredentials
+from azure.storage.blob import BlobServiceClient
 
 from ..db.database import Database
 from ..db.utils import get_db
@@ -17,3 +21,24 @@ def register(guid: str,
         return True
     else:
         raise HTTPException(status_code=400, detail="GUID already exists")
+
+
+@router.post("/data")
+def data(file: UploadFile = File(...)):
+    # TODO: Check if the upload is allowed based on phone number
+
+    # TODO: Check format of the file
+
+    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    container_name = os.getenv("AZURE_STORAGE_CONTAINER")
+
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    # TODO: generate name based on phone number fetched by check
+    directory = random.randrange(0, 9, 1)  # Ideally should be a prefix of phone number
+    blob_file = "./data/" + str(directory) + "/" + file.filename
+
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_file)
+    blob_client.upload_blob(file.file, overwrite=True)
+
+    return True
