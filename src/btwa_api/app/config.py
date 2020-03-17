6c1e5493ad5d2960e0cf4ed04a8b0a7978a01627
Jakub Beránek <berykubik@gmail.com>
2020-03-17 11:21:21 +0100

    Add AzureSQL support

--- src/btwa_api/app/config.py
+++ src/btwa_api/app/config.py
@@ -1,6 +1,7 @@
 import logging
 import pathlib
 import sys
+import urllib
 
 import statsd
 from pyhocon import ConfigFactory
@@ -9,6 +10,10 @@ import os
 
 ROOT_DIR = pathlib.Path(__file__).absolute().parent.parent
 DATABASE_URI = os.environ.get("DATABASE_URI", f"sqlite:///{ROOT_DIR / 'test.db'}")
+
+mssql_prefix = "mssql+pyodbc:///?odbc_connect="
+if DATABASE_URI.startswith(mssql_prefix):
+    DATABASE_URI = f"{mssql_prefix}{urllib.parse.quote_plus(DATABASE_URI[len(mssql_prefix):])}"
 
 config = ConfigFactory.parse_file(ROOT_DIR / "config.conf")
 logger = logging.getLogger(__name__)
