ae8cbc1a3ba777a9aeb8c783be85399e377f1570
Jan Petr <honzik.petr@gmail.com>
2020-03-18 13:32:08 +0100

    the algorithm will be purely additive and take data only from the past 14 days

--- src/btwa_api/app/detection.py
+++ src/btwa_api/app/detection.py
@@ -1,10 +1,6 @@
-SCAN_PERIOD = 2 * 60 * 1000  # 2 minutes in milliseconds
-BREAK_MEETING_TRESHOLD = 60 * 60 * 1000  # 1 hour in milliseconds
-SCORE_DEDUCTION_ON_MEETING_BREAK = 3
-INFECTED_SCORE_TRESHOLD = 100
-
-
 def detect(input_data: list):
+    # TODO: Filter out data older than 14 days
+
     scores = {}
     for data in input_data:
         current_timestamp = data["timestamp"]
@@ -14,38 +10,17 @@ def detect(input_data: list):
         # NEW MEETING
         if buid not in scores:
             scores[buid] = {
+                "buid": buid,
                 "score": get_score(signal),
                 "last_timestamp": current_timestamp
             }
             continue
-
-        # When already infected, skip all future occurrences
-        if scores[buid]["score"] >= INFECTED_SCORE_TRESHOLD:
-            continue
-
-        # ALREADY MET
-        # meeting was broken, counting from 0
-        if current_timestamp - scores[buid]["last_timestamp"] > BREAK_MEETING_TRESHOLD:
-            scores[buid] = {
-                "score": get_score(signal),
-                "last_timestamp": current_timestamp
-            }
-            continue
-
-        # WAS DECIDED NOT TO IMPLEMENT DEDUCTION FOR NOW
-        # every 2 minutes they were apart, reduce score by 3
-        # i = scores[buid]["last_timestamp"] + SCAN_PERIOD
-        # while i < current_timestamp:
-        #     scores[buid]["score"] = max(0, scores[buid]["score"] - SCORE_DEDUCTION_ON_MEETING_BREAK)
-        #     i += SCAN_PERIOD
 
         # add score and update last timestamp
         scores[buid]["score"] += get_score(signal)
         scores[buid]["last_timestamp"] = current_timestamp
 
-    booleanized = decide_infected(scores)
-
-    return booleanized
+    return scores
 
 
 def get_score(signal):
@@ -65,14 +40,6 @@ def get_score(signal):
         return 5
 
     return 0
-
-
-def decide_infected(scores):
-    booleanized = []
-    for buid in scores:
-        booleanized.append({"buid": buid, "infected": scores[buid]["score"] >= INFECTED_SCORE_TRESHOLD})
-
-    return booleanized
 
 
 # list of tuples (timestamp_ms, buid, expositionSeconds, maxRssi, avgRssi, medRssi)
