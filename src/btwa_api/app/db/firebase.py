6fde539fae9a0902cc5e09722214b4cd2230cd9f
Jakub Beránek <berykubik@gmail.com>
2020-03-18 13:25:24 +0100

    Sort proximity records by timestamp in ascending order

--- src/btwa_api/app/db/firebase.py
+++ src/btwa_api/app/db/firebase.py
@@ -32,7 +32,7 @@ def get_user_proximity(bucket, user):
     most_recent = files[0]
     content = most_recent.download_as_string()
     reader = csv.DictReader(StringIO(content.decode()))
-    return list(reader)
+    return sorted(reader, key=lambda record: record["timestamp"])
 
 
 class Firebase:
