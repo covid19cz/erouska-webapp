23784f09f34dbdc6116d03968a0ac6fa54f22815
Jakub Beránek <berykubik@gmail.com>
2020-03-19 16:30:08 +0100

    Add CLI for creating users

--- src/btwa_api/scripts.py
+++ src/btwa_api/scripts.py
@@ -1,4 +1,5 @@
 import bcrypt
+import click
 from app.db.sql.models.handler import Handler
 from app.db.sql.session import Session
 
@@ -10,3 +11,20 @@ def add_handler(username, password):
     session.add(handler)
     session.commit()
     session.close()
+
+
+@click.command("add-handler")
+@click.argument("username")
+@click.argument("password")
+def add_handler_cmd(username, password):
+    add_handler(username, password)
+
+
+@click.group()
+def cli():
+    pass
+
+
+if __name__ == "__main__":
+    cli.add_command(add_handler_cmd)
+    cli()
