2304d4e95737245d4e67d5a7cbd6eca979a80377
zdendahak@gmail.com <zdendahak@gmail.com>
2020-03-26 15:29:38 +0100

    firebase emulator

--- envs_local.sh
+++ envs_local.sh
@@ -1,3 +1,6 @@
 export FIREBASE_DB_URL="https://covid19cz.firebaseio.com/"
 export FIREBASE_STORAGE_BUCKET="covid19cz.appspot.com"
 export GOOGLE_APPLICATION_CREDENTIALS="google-credentials.json"
+
+firebase emulators:start
+
